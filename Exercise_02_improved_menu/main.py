8267be2dfb21cf4f190cc80e29b3724b55a442f4
Marco <marco.pertegal.prieto@gmail.com>
2024-01-14 23:24:31 +0100

    Exercise 2 code documented

--- Exercise_02_improved_menu/main.py
+++ Exercise_02_improved_menu/main.py
@@ -4,14 +4,26 @@ def import_words():
     with open('palabras.txt', 'r') as archivo:
         for linea in archivo:
             palabra = linea.strip()
+            #comprueba si la palabra recorrida en el bucle está en el
+            #diccionario usando in
             if palabra in palabras:
-                palabras[palabra] += 1  # Incrementar la frecuencia si la palabra ya existe
+                #si la palabra se encuentra amumenta el valor de la frecuencia asociada
+                #a esa palabra en uno, la frecuencia es el valor asociado a cada palabra
+                #se agrega 1 al valor asociado a esa palabra en el diccionario.
+                palabras[palabra] += 1
             else:
-                palabras[palabra] = 1  # Agregar la palabra al diccionario con frecuencia 1
+                #Agregar la palabra al diccionario con frecuencia 1
+                palabras[palabra] = 1
     return palabras
 
-def show_words(palabras):
+def show_words():
+    #usamos los pares de valores del diccionario
+    #el .items es un metodo de los diccionarios y podemos extraer los elementos clave valor
     for palabra, frecuencia in palabras.items():
+        #usando la f podemos imprimir variables
+        #para imprimir variables que son int debemos pasarlas a str sino peta
+        #como esto es poco legible se usa f-string para imprimir variables int y str al mismo tiempo
+        #print(palabra+":" +str(frecuencia)+ "veces")
         print(f"{palabra}: {frecuencia} veces")
 
 while True:
@@ -27,7 +39,7 @@ while True:
 
         elif opcion == '2':
             if palabras:
-                show_words(palabras)
+                show_words()
             else:
                 print("No hay palabras claves añadidas")
 
