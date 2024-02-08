c0a19317909f4317067c8f9fa694c3b36cf0ea3e
MarcoPertegal <marco.pertegal.prieto@gmail.com>
2024-02-08 14:22:14 +0100

    fast api but dont recognize uvicorn

---
+++ Python-fastapi/main.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+#pydantic = creacion de dto
+
+app = FastAPI()
+
+
+@app.get("/")
+async def get():
+    return {"message": "Hello World"}
+
+class User(BaseModel):
+    id: int
+    name: str 
+    fecha_alta = datetime | None
+    aficiones = dict[str, PositiveInt]
+
+
+app = FastAPI()
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
